ac23014ec30c513a163435dd7f0c1bd8c92c0711
Cp3o <carolepes@gmail.com>
2025-06-04 16:17:34 +0200

    config: images remotes pattern

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,16 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/media/**',
+      },
+    ],
+  },
 };
 
 
