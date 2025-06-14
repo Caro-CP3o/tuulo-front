bfce33b9ebfc0a065dd77c4ed20d6a9b7ccfe204
Cp3o <carolepes@gmail.com>
2025-06-14 09:47:39 +0200

    trash

--- trash/join-request/page.tsx
+++ trash/join-request/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { joinFamilyRequest } from "../../lib/api";
+import { joinFamilyRequest } from "../../src/lib/api";
 // import { joinFamilyRequest } from "@/lib/api";
 
 export default function JoinRequestForm() {
